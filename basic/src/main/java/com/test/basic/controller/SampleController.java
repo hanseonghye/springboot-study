0a60bd978704bf4f43f23d71bd1cd9b05668ccd4
hanseonghye <hans12312@naver.com>
2020-03-08 14:49:24 +0900

    no message

---
+++ basic/src/main/java/com/test/basic/controller/SampleController.java
@@ -0,0 +1,13 @@
+package com.test.basic.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SampleController {
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
+}
